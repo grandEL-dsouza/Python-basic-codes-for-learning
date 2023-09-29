6336a433bbc01e979946035ca70ad52697298088
GrandEL <60457320+grandEL-dsouza@users.noreply.github.com>
2023-09-29 15:40:17 +0530

    Add files via upload

---
+++ vowelcheck.py
@@ -0,0 +1,9 @@
+a=input("enter the string")
+b=0
+for i in range (0, len(a)):
+    if a[i]=='a' or a[i]=='e' or a[i]=='i' or a[i]=='o' or a[i]=='u':
+        b=b+1
+        print ("vowel is present")
+        i=i+1
+if b==0:
+    print("the string has no vowels")
